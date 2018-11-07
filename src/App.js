cf95bd9041d87fbcc74116271831894dd14b89ce
praveenkhanal <praveen_fren@hotmail.com>
2018-11-07 11:28:10 -0800

    creating components, container and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import MyRoutes from './config/routes';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import TodosContainer from './containers/TodosContainer';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+      <Header/> 
+         { MyRoutes }
       </div>
     );
   }
---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+  render() {
+    return (
+      <h2>
+        this is Todos
+      </h2>
+    );
+  }
+}
+
+export default Todos;
